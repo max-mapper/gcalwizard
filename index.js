829794c0c46dd573bdc953731ba56e7a892be332
Max Ogden <max@maxogden.com>
2016-10-12 13:48:37 +0800

    first version

---
+++ index.js
@@ -0,0 +1,37 @@
+var calurl = require('generate-google-calendar-url')
+var datePick = require('flatpickr')
+
+var startInput = document.querySelector('.startInput')
+var titleInput = document.querySelector('.titleInput')
+var detailsInput = document.querySelector('.detailsInput')
+var tzInput = document.querySelector('.tzInput')
+var linkContainer = document.querySelector('.link')
+
+var start = new Date()
+var tz = Intl.DateTimeFormat().resolvedOptions().timeZone
+tzInput.value = tz
+
+startInput.flatpickr({enableTime: true, defaultDate: start, onChange: function (date) {
+  start = date[0]
+  render()
+}})
+titleInput.addEventListener('change', render)
+detailsInput.addEventListener('change', render)
+tzInput.addEventListener('change', render)
+
+function render (e) {
+  var end = new Date(start)
+  end.setHours(end.getHours() + 1)
+  var url = calurl({
+    start: start,
+    end: end,
+    title: titleInput.value,
+    details: detailsInput.value
+  })
+  var tz = tzInput.value
+  if (tz) url = url + '&ctz=' + tz
+  url = url.replace('http://', 'https://')
+  linkContainer.innerHTML = `<a target="_blank" href="${url}">${url}</a>`
+}
+
+render()
